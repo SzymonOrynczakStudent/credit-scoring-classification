cd0a79a1826e2067fa83e008bb20bcb992e8b0af
SzymonOrynczakStudent <s191722@student.pg.edu.pl>
2025-12-09 10:58:38 +0100

    First final version of the thesis

---
+++ README.md
@@ -0,0 +1,2 @@
+# credit-scoring
+Development of engineering thesis
---
+++ setup.bat
@@ -0,0 +1,6 @@
+if not exist venv (
+    python -m venv venv
+)
+call venv\Scripts\activate
+python -m pip install --upgrade pip
+pip install -r requirements.txt
